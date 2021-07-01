72043211925318274c40929d23f0943e766ae890
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-07-01 16:27:56 -0400

    changed itemsPipeline and added pipeline to settings.py so that it creates json of type House-month-day-year.json every time the spider runs.

--- getRent/getRent/pipelines.py
+++ getRent/getRent/pipelines.py
@@ -6,8 +6,29 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import datetime
+from scrapy.exporters import JsonItemExporter
 
 
-class GetrentPipeline:
+class GetrentPipeline():
+
+    file = None
+
+    def open_spider(self, spider):
+        time_now  = datetime.datetime.now().strftime('%m-%d-%Y') 
+        file_name = f"House-{time_now}.json"
+        self.file = open(file_name, 'wb')
+        self.exporter = JsonItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+# class GetrentPipeline:
+#     def process_item(self, item, spider):
+#         return item
